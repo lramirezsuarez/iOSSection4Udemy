0b75c637a5b6b17be44abbbc3efab41fa2d38baa
Luis Alejandro Ramirez <lramirezsuarez@gmail.com>
2017-05-23 13:08:48 -0500

    First part of first excercise.

---
+++ iOSFeaturesSection4/iOSFeaturesSection4/EggTimerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EggTimerViewController.swift
+//  iOSFeaturesSection4
+//
+//  Created by Luis Ramirez on 5/23/17.
+//  Copyright © 2017 Lucho. All rights reserved.
+//
+
+import UIKit
+
+class EggTimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+}
---
+++ iOSFeaturesSection4/iOSFeaturesSection4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSFeaturesSection4
+//
+//  Created by Luis Ramirez on 5/23/17.
+//  Copyright © 2017 Lucho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
